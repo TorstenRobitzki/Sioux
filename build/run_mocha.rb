7eebac93bf3e36bb3b7861407a1b399a85f07958
= <Torsten@Robitzki.de>
2014-01-17 12:03:32 +0100

    be able to select single test from mocha tests

--- build/run_mocha.rb
+++ build/run_mocha.rb
@@ -4,8 +4,11 @@ def mocha_test_task test_name, *options
     options.each { | p | params.merge! p }
     sources    = params.delete( :sources ).to_s
 
-    task test_name.to_sym do
-        cmd = "mocha --compilers coffee:coffee-script #{sources}"
+    task test_name.to_sym, [ :argument ] do | t, args |
+        single_test = args[ :argument ]
+        testfilter = single_test.nil? ? '' : "-g #{single_test}" 
+        
+        cmd = "mocha --compilers coffee:coffee-script #{testfilter} #{sources}"
         result = system cmd
         
         raise "#{test_name} failed!" unless result
