1e9078d2326bb759c6f61a8c70937bde3052a149
= <Torsten@Robitzki.de>
2014-02-14 16:45:27 +0100

    test names with spaces

--- build/run_mocha.rb
+++ build/run_mocha.rb
@@ -6,7 +6,7 @@ def mocha_test_task test_name, *options
 
     task test_name.to_sym, [ :argument ] do | t, args |
         single_test = args[ :argument ]
-        testfilter = single_test.nil? ? '' : "-g #{single_test}" 
+        testfilter = single_test.nil? ? '' : "-g \"#{single_test}\"" 
         
         cmd = "mocha --compilers coffee:coffee-script #{testfilter} #{sources}"
         result = system cmd
